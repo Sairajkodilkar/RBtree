db74706fdce11d5c336ff17e3d7e78535b9da120
Sairaj Kodilkar <skodilkar7@gmali.com>
2021-01-20 00:21:57 +0530

    Deletion added

--- RBtree.c
+++ RBtree.c
@@ -124,7 +124,155 @@ void RBtreeInsert(RBtree *tree, int data){
 	return;
 }
 
+/* Replace dest by src and free the dest */
+void RBtreeTransplant(RBtree *tree, node *dest, node *src){
+	if(dest == tree->root)
+		tree->root = src;
+	else if(dest == dest->p->l)
+		dest->p->l = src;
+	else
+		dest->p->r = src;
+	src->p = dest->p;
+
+	return ;
+}
+
+node *RBtreeMinimal(RBtree *tree, node *nd){
+	node *x, *y;
+	x = tree->nil;
+	y = nd;
+	//x follows y
+	while(y != tree->nil){
+		x = y;
+		y = y->l;
+	}
+	return x;
+}
+node *RBtreeMaximal(RBtree *tree, node *nd){
+	node *x, *y;
+	x = tree->nil;
+	y = nd;
+	//x follows y
+	while(y != tree->nil){
+		x = y;
+		y = y->r;
+	}
+	return x;
+}
+
+void RBtreeFixup(RBtree *tree, node *x) {
+	while(x != tree->root && x->color == BLACK){
+		node *sibling;
+		if(x == x->p->r){
+			sibling = x->p->l;
+			if(sibling->color == RED){ //convert case 1 to case 2, 3 or 4
+				//we need to switch colors before rotations so that it does not create new violation
+				//starting from x->p
+				sibling->color = BLACK;
+				x->p = RED;
+				leftRotate(tree, x->p);
+				sibling = x->p->r;
+
+			}
+			if(sibling->l->color == BLACK && sibling->r->color == BLACK){
+				sibling->color = RED;
+				x = x->p; //when sibling becomes red then parent violets the condition instead of x
+			}
+			else {
+				if(sibling->r->color == BLACK){
+					sibling->l->color = BLACK; 	//convert case 3 to case 4 by rotating about sibling and making it red
+					sibling->color = RED;
+					rightRotate(tree, sibling);
+					sibling = x->p->l;
+				}
+				sibling->color = sibling->p->color; //sibling color is taken as parent color to make black count same after rotation
+				sibling->p->color = BLACK; //Make parent color black to accept left child of sibling after rotation
+				sibling->r->color = BLACK;
+				leftRotate(tree, sibling->p);
+				x = tree->root;
+			}
+		}
+
+		else{
+			sibling = x->p->r;
+			if(sibling->color == RED){ //convert case 1 to case 2, 3 or 4
+				//we need to switch colors before rotations so that it does not create new violation
+				//starting from x->p
+				sibling->color = BLACK;
+				x->p = RED;
+				rightRotate(tree, x->p);
+				sibling = x->p->l;
+
+			}
+			if(sibling->r->color == BLACK && sibling->l->color == BLACK){
+				sibling->color = RED;
+				x = x->p; //when sibling becomes red then parent violets the condition instead of x
+			}
+			else {
+				if(sibling->l->color == BLACK){
+					sibling->r->color = BLACK; 	//convert case 3 to case 4 by rotating about sibling and making it red
+					sibling->color = RED;
+					leftRotate(tree, sibling);
+					sibling = x->p->r;
+				}
+				sibling->color = sibling->p->color; //sibling color is taken as parent color to make black count same after rotation
+				sibling->p->color = BLACK; //Make parent color black to accept left child of sibling after rotation
+				sibling->l->color = BLACK;
+				rightRotate(tree, sibling->p);
+				x = tree->root;
+			}
+		}
+	}
+
+	x->color = BLACK;
+}
+
 
 void RBtreeDelete(RBtree *tree, int data){
+	node *x = tree->nil, *y = tree->root, *temp;
+	while(y != tree->nil) {
+		x = y;
+		if(data > y->data) {
+			y = y->r;
+		}
+		else if(data < y->data) {
+			y = y->l;
+		}
+		else {
+			break;
+		}
+	}
+
+	int ycolor = y->color;
+	if(y->l == tree->nil){
+		x = y->r;
+		RBtreeTransplant(tree, y, y->r);
+		free(y);
+	}
+	else if(y->r == tree->nil){
+		x = y->l;
+		RBtreeTransplant(tree, y, y->l);
+		free(y);
+	}
+	else {
+		node *mini = RBtreeMinimal(tree, y->r);
+		ycolor = mini->color;
+		if(mini != y->r){
+			RBtreeTransplant(tree, mini, mini->r);
+			mini->r = y->r;
+			mini->r->p = mini;
+		}
+
+		RBtreeTransplant(tree, y, mini);
+		mini->l = y->l;
+		mini->color = y->color;
+		x = mini->r;
+
+		free(y);
+
+	}
+
+	if(ycolor == BLACK)
+		RBtreeFixup(tree, x);
 }
 
--- RBtree.h
+++ RBtree.h
@@ -5,6 +5,13 @@
 
 /* RED-BLACK tree implimentation 
  * Reference: CLRS page 309-338
+ *
+ * RB tree has following properties which is to be maintained
+ * 	1. Every node is either red or black.
+ * 	2. The root is black.
+ * 	3. Every leaf (NIL) is black.
+ * 	4. If a node is Red, then both its children are black.
+ * 	5. For each node, all simple paths from the node to descendant leaves constains the same number of black nodes.
  */
 
 
